963850d314c5bef7e01074fb2acff3484a0cde58
SanityaPurnomo <m26415005@john.petra.ac.id>
2016-10-18 08:25:23 +0700

    history4_minggu5

---
+++ history4_minggu5.pl
@@ -0,0 +1,14 @@
+#!/usr/bin/perl
+use warnings;
+
+print"Hello\nPlease input string: ";
+$str = <>;
+print "input angka : ";
+$angka = <>;
+chomp($str);
+chomp($angka);
+
+print "$str adalah $angka\n";
+print $str . " adalah " .$angka . " \n";
+print 3+7 . "\n";
+print "$str\n" x 5 . "\n";
