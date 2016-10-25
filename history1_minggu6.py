caf32e975c1edf6e5755c06d0978a86e4d043cad
SanityaPurnomo <m26415005@john.petra.ac.id>
2016-10-25 08:00:55 +0700

    history2_minggu6

--- history1_minggu6.py
+++ history1_minggu6.py
@@ -7,3 +7,24 @@ def main() :
 
 if __name__ == '__main__' :
 	main();
+
+
+
+
+#!/usr/bin/python
+
+def main() :
+        x = 5;
+        y = 10;
+        print('The value of x before swapping : {}'.format(x));
+        print('The value of y before swapping : {}'.format(y));
+
+        temp =  x;
+        x = y;
+        y = temp;
+
+        print('The value of x after swapping : {}'.format(x));
+        print('The value of y after swapping: {}'.format(y));
+
+if __name__ == '__main__':
+        main()
