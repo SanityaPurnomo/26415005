6711f840d6458579d75c23d501e33a431b472aa8
SanityaPurnomo <m26415005@john.petra.ac.id>
2016-10-25 08:17:19 +0700

    history6_minggu4

--- history1_minggu6.py
+++ history1_minggu6.py
@@ -50,3 +50,25 @@ def main() :
 if __name__ == '__main__' :
         main();
 
+
+
+#!usr/bin/python
+
+def factorial(n):
+        if n == 1:
+                return n;
+        else:
+                return n*factorial(n-1);
+
+def main() :
+        num = int(input("Enter a number: "));
+        if num < 0:
+                print"Sorry, factorial does not exist for negative numbers";
+        elif num == 0:
+                print"The factorial of 0 is 1";
+        else:
+                print'The factorial of',num,'is',factorial(num);
+
+if __name__ == '__main__' :
+        main();
+
