3944f06c74b70339332c58578cec65252d48879f
SanityaPurnomo <m26415005@john.petra.ac.id>
2016-11-01 09:49:47 +0700

    history4_minggu7

--- history_minggu7.py
+++ history_minggu7.py
@@ -60,3 +60,20 @@ def main() :
 
 if __name__ == '__main__' :
 	main();
+
+
+
+#!/usr/bin/python
+
+def main() :
+
+	prices = {'apple': 0.40, 'banana': 0.50}
+	my_purchase = {
+    		'apple': 1,
+    		'banana': 6}
+	grocery_bill = sum(prices[fruit] * my_purchase[fruit]
+                   for fruit in my_purchase)
+print 'I owe the grocer $%.2f' % grocery_bill
+
+if __name__ == '__main__' :
+	main();
