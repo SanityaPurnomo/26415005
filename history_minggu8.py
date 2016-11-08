7b071b4720d93d866e90bc56a794179898dacf6f
SanityaPurnomo <m26415005@john.petra.ac.id>
2016-11-08 08:52:58 +0700

    history3_minggu8

--- history_minggu8.py
+++ history_minggu8.py
@@ -45,3 +45,31 @@ def solve(n):
                 if not under_attack(i+1, solution)]
 for answer in solve(BOARD_SIZE):
     print answer
+
+
+
+
+
+
+
+
+import itertools
+
+def iter_primes():
+     # an iterator of all numbers between 2 and +infinity
+     numbers = itertools.count(2)
+
+     # generate primes forever
+     while True:
+         # get the first number from the iterator (always a prime)
+         prime = numbers.next()
+         yield prime
+
+         # this code iteratively builds up a chain of
+         # filters...slightly tricky, but ponder it a bit
+         numbers = itertools.ifilter(prime.__rmod__, numbers)
+
+for p in iter_primes():
+    if p > 1000:
+        break
+    print p
