03ecd3a5c0a1314c9324b526b822df2012dd9b82
SanityaPurnomo <m26415005@john.petra.ac.id>
2016-11-08 08:52:14 +0700

    history2_minggu8

--- history_minggu8.py
+++ history_minggu8.py
@@ -14,3 +14,34 @@ status_labels = {-1: 'down', 0: 'unchanged', 1: 'up'}
 for ticker, name, price, change, pct in stocks:
     status = status_labels[cmp(float(change), 0.0)]
     print '%s is %s (%s%%)' % (name, status, pct)
+
+
+
+
+
+
+
+BOARD_SIZE = 8
+
+def under_attack(col, queens):
+    left = right = col
+
+    for r, c in reversed(queens):
+        left, right = left - 1, right + 1
+
+        if c in (left, col, right):
+            return True
+    return False
+
+def solve(n):
+    if n == 0:
+        return [[]]
+
+    smaller_solutions = solve(n - 1)
+
+    return [solution+[(n,i+1)]
+        for i in xrange(BOARD_SIZE)
+            for solution in smaller_solutions
+                if not under_attack(i+1, solution)]
+for answer in solve(BOARD_SIZE):
+    print answer
