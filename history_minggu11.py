d663e39a647a254cccc9490cbde05ced531d2fc0
SanityaPurnomo <m26415005@john.petra.ac.id>
2016-11-29 08:00:32 +0700

    history1_minggu11

---
+++ history_minggu11.py
@@ -0,0 +1,28 @@
+--coba bokeh isi views.py--
+from django.http import HttpResponse, Http404
+from django.shortcuts import render,get_object_or_404
+#from django.views import generic
+from .models import Cabe, Bulan, Harga
+from bokeh.plotting import figure
+from bokeh.resources import CDN
+from bokeh.embed import components
+
+def simple_chart(request):
+    plot = figure()
+    plot.circle([1,2],[3,4])
+    script, div = components(plot, CDN)
+    context = {'the_script':script, 'the_div':div}
+    return render(request, 'priceAnalysis/simple_chart.html', context)
+
+def index(request):
+    return render(request, 'priceAnalysis/index.html')
+
+def detail(request, cabe_id):
+    cabe = get_object_or_404(Cabe, pk=cabe_id)
+    context = {'cabe' : cabe}
+    return render(request, 'priceAnalysis/detail.html', context)
+
+def list(request):
+    all_cabe = Cabe.objects.all()
+    context = {'all_cabe': all_cabe}
+    return render(request,'priceAnalysis/listdata.html',context)
