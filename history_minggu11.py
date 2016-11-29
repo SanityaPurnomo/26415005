64bfbdc3f9e502e007a546e748d32c1fcbb4c42f
SanityaPurnomo <m26415005@john.petra.ac.id>
2016-11-29 08:09:21 +0700

    history5_minggu11

--- history_minggu11.py
+++ history_minggu11.py
@@ -79,3 +79,23 @@ plot = figure()
 plot.circle([1,2], [3,4])
 
 html = file_html(plot, CDN, "my plot")
+
+
+
+
+
+--coba graph di bokeh
+from bokeh.plotting import figure, output_file, show
+
+x = [1, 2, 3, 4, 5]
+y = [6, 7, 8, 7, 3]
+
+output_file("multiple.html")
+
+p = figure(plot_width=400, plot_height=400)
+
+# add both a line and circles on the same plot
+p.line(x, y, line_width=2)
+p.circle(x, y, fill_color="white", size=8)
+
+show(p)
