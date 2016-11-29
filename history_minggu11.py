5602456e3f5168a5f1d4d28f67b9d213f2ae2af4
SanityaPurnomo <m26415005@john.petra.ac.id>
2016-11-29 08:02:47 +0700

    history4_minggu11

--- history_minggu11.py
+++ history_minggu11.py
@@ -65,3 +65,17 @@ urlpatterns = [
 
 </body>
 </html>
+
+
+
+
+
+--coba standalone html bokeh--
+rom bokeh.plotting import figure
+from bokeh.resources import CDN
+from bokeh.embed import file_html
+
+plot = figure()
+plot.circle([1,2], [3,4])
+
+html = file_html(plot, CDN, "my plot")
